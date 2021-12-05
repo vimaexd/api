e2b242ff7fd7e5c5787b7508d5d71a94932a5a7f
etstringy <eddyteddyt@gmail.com>
2021-12-05 21:14:34 +0000

    eshop

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import Log from './utils/Log';
 import Twitch from './routes/Twitch';
 import Discord from './routes/Discord';
 import PetPet from './routes/PetPet';
+import EShop from './routes/EShop';
 
 console.log(gradient.pastel(figlet.textSync('STRINGY', 'Univers')));
 dotenv.config();
@@ -29,19 +30,25 @@ const l = new Log({prefix: 'API', color: 'magenta'});
 const twitch = new Twitch();
 const discord = new Discord();
 const petpet = new PetPet();
+const lawyered = new EShop();
 
 /*
     ROUTES
 */
 
 app.get('/', (req: Request, res: Response) => res.json({success: true, nyaa: 'owo'}));
+
+/* Twitch */
 app.get('/twitch/:channel', limiter, (req: Request, res: Response) => twitch.getChannel(req, res));
+
+/* Discord */
 app.get('/discord/user/:id', limiter, (req: Request, res: Response) => discord.getUserRoute(req, res));
 app.get('/pet/:id', (req: Request, res: Response) => petpet.generateGif(req, res));
-app.get('/misc/naniactivation/:orderId/:email/:idk', (req: Request, res: Response) => {
-  console.log(req.body);
-  res.json({body: 'owo', validated: true});
-});
+
+/* EShop */
+app.get('/eshop/all', (req: Request, res: Response) => lawyered.allRoute(req, res));
+app.get('/eshop/search/', (req: Request, res: Response) => lawyered.searchRoute(req, res));
+app.get('/eshop/music', (req: Request, res: Response) => lawyered.randomMusicRoute(req, res));
 
 app.listen(9090, () => {
   l.log(`Stringy Software API up`);
---
+++ src/routes/EShop.ts
@@ -0,0 +1,109 @@
+import {Request, Response} from 'express';
+import fs from 'fs';
+import path from 'path';
+import axios, {AxiosInstance} from 'axios';
+import Log from '../utils/Log';
+
+const l = new Log({prefix: 'EShop'});
+
+/**
+ * Class for getting Twitch channel info for the API
+ */
+export default class EShop {
+    cacheFolder: string;
+    cachePath: string;
+    eshopData: any;
+
+    /**
+     * Initialize class variables
+     */
+    constructor() {
+      this.eshopData = [{
+        docs: []
+      }];
+      this.initialize();
+    }
+
+    /**
+     * Initialize function to run other functions asyncronously
+     */
+    private async initialize() {
+      this.cacheFolder = path.join(__dirname, '../../', 'cache', 'eshop');
+      this.cachePath = path.join(this.cacheFolder, "data.json");
+      fs.mkdirSync(this.cacheFolder, {recursive: true});
+
+      if(!fs.existsSync(this.cachePath)){
+        fs.writeFileSync(this.cachePath, this.eshopData, {encoding: "utf-8"})
+      } else {
+        this.eshopData = JSON.parse(fs.readFileSync(this.cachePath, {encoding: "utf-8"}));
+      }
+      
+      // this.updateGames();
+      l.log("E-Shop started")
+    }
+
+    updateGames() {
+      const url = 'http://search.nintendo-europe.com/en/select';
+      const staticArgs = 'fq=type%3AGAME%20AND%20system_type%3Anintendoswitch*%20AND%20product_code_txt%3A*&q=*&sort=sorting_title%20asc&start=0&wt=json&'
+      const rows = 10000
+      
+      const res = axios.get(
+        url + "?" + staticArgs + `rows=${rows}`
+      )
+      .then(res => {
+        this.eshopData = res.data.response
+        fs.writeFileSync(this.cachePath, JSON.stringify(res.data.response), {encoding: "utf-8"})
+        l.log(`E-Shop data updated! (${this.eshopData.docs.length} games)`)
+      })
+      .catch(err => {
+        console.log(err)
+        l.log("Error updating E-Shop data!")
+      })
+    }
+
+    searchRoute(req: Request, res: Response) {
+      if (this.eshopData.docs.length === 0) {
+        return res.json({
+          success: false,
+          description: 'No EShop data serverside',
+        });
+      }
+
+      if (!req.query.q) {
+        return res.json({
+          success: false,
+          description: 'Invalid search query',
+        });
+      }
+
+      const data = this.eshopData.docs
+        .filter((item: any) => (
+          item.title
+          .toLowerCase()
+          .includes((req.query.q as string).toLowerCase())
+        ));
+      return res.send(data)
+    }
+
+    allRoute(req: Request, res: Response) {
+      if (this.eshopData.length === 0) {
+        return res.json({
+          success: false,
+          description: 'No EShop data serverside',
+        });
+      }
+
+      return res.json(this.eshopData.docs)
+    }
+
+    randomMusicRoute(req: Request, res: Response) {
+      const musicPath = path.join(__dirname, "..", "music", "eshop");
+      const music = fs.readdirSync(musicPath)
+
+      const track = music[Math.floor(Math.random() * music.length)];
+      res.setHeader('Content-Type', 'audio/flac')
+      res.sendFile(
+        path.join(musicPath, track)
+      )
+    }
+}
